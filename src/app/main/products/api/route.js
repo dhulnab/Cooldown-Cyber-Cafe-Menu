b8101ad6969aa3db279f1d16b7c250e1f9c64997
Dhulfiqar Ali <dhulfiqarali6@gmail.com>
2024-07-29 09:54:42 +0300

    fixing some bugs 2

--- src/app/main/products/api/route.js
+++ src/app/main/products/api/route.js
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
+
+export const dynamic = "force-dynamic";
 export async function GET() {
   const excludedCategories = ["offer", "entertainment"];
   const items = await db.product.findMany({
